a75a701ed9b6ec54052d3939b3681eb8fdabc738
franra18 <franramirez@uma.es>
2025-10-02 21:05:38 +0200

    Realización ejercicios 9-12

---
+++ Prácticas Arduino/ej11/ej11.ino
@@ -0,0 +1,19 @@
+const int sensorPin = A0;
+float sensorValue = 0;
+int pinLed = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+
+  // La resolución del ADC es de 10 bits (0-1023)
+  // para una referencia de 255
+
+  float intensidad = sensorValue * (255.0 / 1023.0);
+  analogWrite(pinLed, intensidad);
+}
---
+++ Prácticas Arduino/ej12/ej12.ino
@@ -0,0 +1,35 @@
+#include <math.h>
+
+const int sensorPin = A0;
+float sensorValue = 0;
+int pinLed = 3;
+int ultimaLectura = 0;
+int periodoLectura = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinLed, OUTPUT);
+}
+
+float celsius(float sensorValue) {
+  float a, b, rtmp;
+  a = (1.0 / (25.0 + 273.15));
+  rtmp = ((1023.0 * 10e4) / sensorValue) - 10e4;
+  b = (1.0 / 3977.0) * log(rtmp / 10e4);
+  return (1.0/(a+b))-273.15;
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+
+  unsigned long num = millis();
+
+  if (num - ultimaLectura >= periodoLectura) {
+    if (celsius(sensorValue) >= 36 && celsius(sensorValue) >= 40) {
+      digitalWrite(pinLed, HIGH);
+    } else {
+      digitalWrite(pinLed, LOW);
+    }
+    Serial.println(celsius(sensorValue));
+  }
+}
